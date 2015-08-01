356d98cf70c334b714c41cc4fb05016d290f10cf
lutskovsky <sergius.lutskovsky@gmail.com>
2015-08-01 15:19:47 +0300

    bcm hardcode

--- config.py
+++ config.py
@@ -5,13 +5,11 @@ fontsize = 500
 log_dir = 'log'
 fullscreen = False # if True window title bar is hidden
 
-numbering_mode = 'GPIO.BCM' # 'GPIO.BCM' or 'GPIO.BOARD'
-
 # Bouncetime (in milliseconds) is the delay before accepting new input on the same channel.
-# You can set up a single default bouncetime and individual bouncetime for single channels (optional).
+# You can set up a single default bouncetime and an individual bouncetime for each channel (optional).
 default_bouncetime = 200
 
-# Pin - number according to numbering_mode; effect - 'increase', 'decrease' or 'reset';
+# Pin - GPIO pin number; effect - 'increase', 'decrease' or 'reset';
 # bouncetime - bouncetime for this pin (if omitted, default_bouncetime is used)
 pins = (
     # (pin, effect, bouncetime)
--- counter.py
+++ counter.py
@@ -73,7 +73,7 @@ except (IOError, ValueError):
 finally:
     total = open('total', 'w+', 0)
 
-GPIO.setmode(globals()[numbering_mode])
+GPIO.setmode(GPIO.BCM)
 
 for settings in pins:
     setup_pins(*settings)
